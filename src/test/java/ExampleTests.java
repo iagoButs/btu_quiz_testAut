35109b9f0361a666aca0e54c10dbaa961f07c514
iagoButs <iagobutskhrikidze@gmail.com>
2022-10-02 18:44:37 +0400

    finished

--- src/test/java/ExampleTests.java
+++ src/test/java/ExampleTests.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -14,11 +15,25 @@ public class ExampleTests {
         WebDriver driver = new ChromeDriver();
         driver.get("http://the-internet.herokuapp.com/");
         driver.manage().window().maximize();
-        WebElement button=driver.findElement(By.xpath("//ul/li/a"));
+
+        WebElement button=driver.findElement(By.xpath("//ul/li[1]/a"));
         button.click();
         WebElement text = driver.findElement(By.xpath("//*[@id=\"content\"]/div/h3"));
-        Assert.assertEquals(text.getText(),"No Test");
+        //Assert.assertEquals(text.getText(),"A/B Test Variation 1");
 
+
+        driver.navigate().back();
+
+        WebElement drp_down= driver.findElement(By.linkText("Dropdown"));
+        drp_down.click();
+
+        WebElement drp_select_opt =driver.findElement(By.xpath("//select"));
+        Select select=new Select(drp_select_opt);
+        select.selectByVisibleText("Option 1");
+
+        Assert.assertEquals(select.getFirstSelectedOption().getText(),"Option 1" );
+        
+        driver.close();
 
 
     }
